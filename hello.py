cea990ffbe6459ed05b31673e52a72fffc2cdb0f
lui01212 <40736418+lui01212@users.noreply.github.com>
2023-06-24 09:55:14 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,22 +1,21 @@
 from flask import Flask
 import subprocess
+from subprocess import Popen, PIPE
+from subprocess import check_output
+
+def get_shell_script_output_using_communicate():
+    session = Popen(['./some.sh'], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = session.communicate()
+    if stderr:
+        raise Exception("Error "+str(stderr))
+    return stdout.decode('utf-8')
+
+def get_shell_script_output_using_check_output():
+    stdout = check_output(['/home/buivanluyn/content2/vietTTS/scripts/quick_start.sh']).decode('utf-8')
+    return stdout
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    text = """
-    Đoạn trường tân thanh, thường được biết đến với cái tên đơn giản là Truyện Kiều, là một truyện thơ của đại thi hào Nguyễn Du. 
-    Đây được xem là truyện thơ nổi tiếng nhất và xét vào hàng kinh điển trong văn học Việt Nam.
-    Câu chuyện dựa theo tiểu thuyết Kim Vân Kiều truyện của Thanh Tâm Tài Nhân, một thi sĩ thời nhà Minh, Trung Quốc.
-    Tác phẩm kể lại cuộc đời, những thử thách và đau khổ của Thúy Kiều, một phụ nữ trẻ xinh đẹp và tài năng, phải hy sinh thân mình để cứu gia đình. 
-    Để cứu cha và em trai khỏi tù, cô bán mình kết hôn với một người đàn ông trung niên, không biết rằng anh ta là một kẻ buôn người, và bị ép làm kĩ nữ trong lầu xanh.
-    """
-    command_tts = f'cd /home/buivanluyn/content2/vietTTS; python3 -m vietTTS.synthesizer --lexicon-file assets/infore/lexicon.txt --text="bùi văn luyn" --output=clip.wav --silence-duration 0.2'
-
-    try:
-        result_tts = subprocess.check_output(
-            [command_tts], shell=True)
-    except subprocess.CalledProcessError as e:
-        return "An error occurred while trying to fetch task status updates."
-
-    return 'cd %s, tts %s' % (result_cd, result_tts)
+    return '<pre>'+get_shell_script_output_using_check_output()+'</pre>'
