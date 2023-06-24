2b4d871cc8a868646405e1711b29e45dc7636ec0
lui01212 <40736418+lui01212@users.noreply.github.com>
2023-06-24 10:29:04 +0700

    Update quick_start.sh

--- quick_start.sh
+++ quick_start.sh
@@ -1,4 +1,3 @@
 echo "Generate audio clip"
 text=`cat transcript.txt`
-cd /home/buivanluyn/content2/vietTTS/
 python3 -m vietTTS.synthesizer --text "$text" --output assets/infore/clip.wav --lexicon-file assets/infore/lexicon.txt --silence-duration 0.2
