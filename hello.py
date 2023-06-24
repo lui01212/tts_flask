381c0f63a91bc8ee0ed721e96e2f805baa5ceb13
lui01212 <40736418+lui01212@users.noreply.github.com>
2023-06-24 09:07:30 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 from flask import Flask
+import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    text = """
+    Đoạn trường tân thanh, thường được biết đến với cái tên đơn giản là Truyện Kiều, là một truyện thơ của đại thi hào Nguyễn Du. 
+    Đây được xem là truyện thơ nổi tiếng nhất và xét vào hàng kinh điển trong văn học Việt Nam.
+    Câu chuyện dựa theo tiểu thuyết Kim Vân Kiều truyện của Thanh Tâm Tài Nhân, một thi sĩ thời nhà Minh, Trung Quốc.
+    Tác phẩm kể lại cuộc đời, những thử thách và đau khổ của Thúy Kiều, một phụ nữ trẻ xinh đẹp và tài năng, phải hy sinh thân mình để cứu gia đình. 
+    Để cứu cha và em trai khỏi tù, cô bán mình kết hôn với một người đàn ông trung niên, không biết rằng anh ta là một kẻ buôn người, và bị ép làm kĩ nữ trong lầu xanh.
+    """
+    command_cd = "cd /home/buivanluyn/content2/vietTTS"
+    command_tts = "python3 -m vietTTS.synthesizer --lexicon-file assets/infore/lexicon.txt --text="{text}" --output=clip.wav --silence-duration 0.2"
+
+    try:
+        result_cd = subprocess.check_output(
+            [command_cd], shell=True)
+        result_tts = subprocess.check_output(
+            [command_tts], shell=True)
+    except subprocess.CalledProcessError as e:
+        return "An error occurred while trying to fetch task status updates."
+
+    return return 'cd %s, tts %s % (result_cd, result_tts)
