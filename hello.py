cda55e9dcccd41a3e2cd3ba33d3195070c981ae0
lui01212 <40736418+lui01212@users.noreply.github.com>
2023-07-19 22:22:04 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -55,6 +55,7 @@ def add_guide(text):
         for i in range(len(text_cut)):
             text_to_speech(text_cut[i], f'clip{i}.mp3')
             print(i)
+            print(text_cut[i])
             AudioSegment.from_file(f'./clip{i}.mp3', format="mp3").export(f'./clip{i}.wav', format="wav")
 
         combined_sounds = AudioSegment.from_wav(f'clip0.wav')
